5dcfc02635201638ef43c3a31d5dbf71b1249205
alishapokhrel <alishapokhrel011@gmail.com>
2026-09-23 12:27:50 +0545

    Update top_5_demanded_skills.sql

--- project_1/top_5_demanded_skills.sql
+++ project_1/top_5_demanded_skills.sql
@@ -1,3 +1,11 @@
+/*Question: What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market,
+  providing insights into the most valuable skills for job seekers.
+*/ 
+
 select count(sd.skill_id) as demanded_count, sd.skills 
 from job_postings_fact jpf 
 inner join skills_job_dim sjd 
